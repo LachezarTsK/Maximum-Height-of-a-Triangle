dfbfcac4bfa24a3907424c806e25010d0c988ddc
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-08-31 12:20:43 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,40 @@
+
+/**
+ * @param {number} red
+ * @param {number} blue
+ * @return {number}
+ */
+var maxHeightOfTriangle = function (red, blue) {
+    return Math.max(calculateMaxHeightOfTriangle(red, blue), calculateMaxHeightOfTriangle(blue, red));
+};
+
+/**
+ * @param {number} numberOfBalls
+ * @return {number}
+ */
+function calculateMaxNumberOfOddRowsStartingFromOne(numberOfBalls) {
+    return Math.floor(Math.sqrt(numberOfBalls));
+}
+
+/**
+ * @param {number} numberOfBalls
+ * @return {number}
+ */
+function calculateMaxNumberOfEvenRowsStartingFromTwo(numberOfBalls) {
+    return Math.floor((-1 + Math.floor(Math.sqrt(1 + 4 * numberOfBalls))) / 2);
+}
+
+/**
+ * @param {number} firstColor
+ * @param {number} secondColor
+ * @return {number}
+ */
+function calculateMaxHeightOfTriangle(firstColor, secondColor) {
+    const numberOfRowsWithFirstColorStaringFromOne = calculateMaxNumberOfOddRowsStartingFromOne(firstColor);
+    const numberOfRowsWithSecondColorStartingFromTwo = calculateMaxNumberOfEvenRowsStartingFromTwo(secondColor);
+
+    if (numberOfRowsWithFirstColorStaringFromOne <= numberOfRowsWithSecondColorStartingFromTwo) {
+        return numberOfRowsWithFirstColorStaringFromOne * 2;
+    }
+    return 2 * numberOfRowsWithSecondColorStartingFromTwo + 1;
+}
